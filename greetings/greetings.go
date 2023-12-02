493b0b0c59805de96b67303a295b9b67a272c80a
who-is-hu <dydgndlaek97@gmail.com>
2023-12-02 17:41:21 +0900

    handle error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,16 @@ package main
 import (
 	"example.com/greetings"
 	"fmt"
+	"log"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
